5634ab614a7b4eddee524afc122940c2973fba13
dallanoe <enzo.dalla-nora@etu.unice.fr>
2018-11-26 16:45:52 +0100

    what_if_i_remove command converted with new domain model, adapted for the WEB and obselete shell reference removed

--- main/src/main/java/fr/unice/polytech/cli/commands/whatif/WhatIfIRemoveStory.java
+++ main/src/main/java/fr/unice/polytech/cli/commands/whatif/WhatIfIRemoveStory.java
@@ -2,15 +2,17 @@ package fr.unice.polytech.cli.commands.whatif;
 
 import fr.unice.polytech.cli.framework.Command;
 import fr.unice.polytech.environment.Environment;
+import fr.unice.polytech.graphviz.Sprint;
+import fr.unice.polytech.graphviz.UserStory;
 import fr.unice.polytech.repository.DTORepository;
-import fr.unice.polytech.repository.dto.SprintStatDTO;
-import fr.unice.polytech.repository.dto.SprintWithStoriesDTO;
-import fr.unice.polytech.repository.dto.StoryDTO;
+import fr.unice.polytech.web.CmdException;
+import fr.unice.polytech.web.WebCommand;
 
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class WhatIfIRemoveStory extends Command<Environment> {
+public class WhatIfIRemoveStory extends Command<Environment> implements WebCommand {
 
     private String sprintName;
     private List<String> storyNames;
@@ -20,41 +22,41 @@ public class WhatIfIRemoveStory extends Command<Environment> {
     public String identifier() { return "what_if_i_remove"; }
 
     @Override
+    public Response execResponse() throws CmdException {
+        return Response.ok(execWithResponse()).build();
+    }
+
+    @Override
     public void execute() {
-        System.out.println();
-        DTORepository repository = this.shell.system.getRepository();
-        SprintWithStoriesDTO sprint = repository.getSprintWithStories(sprintName);
+        print(execWithResponse());
+    }
+
+    private String execWithResponse() {DTORepository repository = DTORepository.get();
+        Sprint sprint = repository.getSprint(sprintName);
+
+        repository.fill(sprint);
+
         if(sprint == null){
-            System.out.println("Le spring "+ sprintName+" n'existe pas");
-            System.out.println();
-            return;
+            return "Le sprint "+ sprintName+" n'existe pas\n";
         }
-/*
-        List<StoryDTO> stories = repository.getStoriesIn(storyNames);
+
+        List<UserStory> stories = repository.getStoriesIn(storyNames);
         if(stories.size() != storyNames.size()){
-             List<String> temp = stories.stream().map(StoryDTO::getName).collect(Collectors.toList());
+            List<String> temp = stories.stream().map(UserStory::getName).collect(Collectors.toList());
             String sts = storyNames.stream().filter(x -> !temp.contains(x)).collect(Collectors.joining(", "));
-            System.out.println("Les stories "+sts+" n'existent pas");
-            System.out.println();
-            return;
+            return "Les stories "+sts+" n'existent pas\n";
         }
 
-        List<StoryDTO> notContained = stories.stream().filter( x -> !sprint.getStories().contains(x)).collect(Collectors.toList());
+        List<UserStory> notContained = stories.stream().filter( x -> !sprint.getStoryList().contains(x)).collect(Collectors.toList());
         if(notContained.size() > 0){
-            String sts = notContained.stream().map(StoryDTO::getName).collect(Collectors.joining(", "));
-            System.out.println("Les stories " + sts + " n'appartiennent pas au Sprint " + sprintName);
-            System.out.println();
-            return;
+            String sts = notContained.stream().map(UserStory::getName).collect(Collectors.joining(", "));
+            return "Les stories " + sts + " n'appartiennent pas au Sprint " + sprintName;
         }
 
-
-        SprintStatDTO sprintStat = repository.getSprintStat(sprint.getSprint().getName());
-        int newBv = sprintStat.getBusinessValue() - stories.stream().mapToInt(StoryDTO::getBusinessValue).sum() ;
-        int newSp = sprintStat.getStoryPoints() - stories.stream().mapToInt(StoryDTO::getStoryPoints).sum() ;
-        System.out.println("[Actuellement     ] --> Business Value : " + sprintStat.getBusinessValue() + " - Story Points : " + sprintStat.getStoryPoints());
-        System.out.println("[Apres Suppression]  --> Business Value : " + newBv + " - Story Points : " + newSp);
-        System.out.println();
-*/
+        int newBv = sprint.calculateTotalBusinessValue() - stories.stream().mapToInt(UserStory::getBusinessValue).sum() ;
+        int newSp = sprint.calculateTotalStoryPoints() - stories.stream().mapToInt(UserStory::getStoryPoints).sum() ;
+        return "[Actuellement     ] --> Business Value : " + sprint.calculateTotalBusinessValue() + " - Story Points : " + sprint.calculateTotalStoryPoints() +
+        "\n[Apres Suppression]  --> Business Value : " + newBv + " - Story Points : " + newSp + "\n";
     }
 
 
