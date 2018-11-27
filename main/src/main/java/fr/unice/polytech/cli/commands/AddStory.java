14b415f8afe3dea5dc45c34875e89e0289d169e2
dallanoe <enzo.dalla-nora@etu.unice.fr>
2018-11-27 10:19:33 +0100

    add_stor, remove_story and sort_backlog_by_complexity command converted with new domain model, adapted for the WEB and obselete shell reference removed

--- main/src/main/java/fr/unice/polytech/cli/commands/AddStory.java
+++ main/src/main/java/fr/unice/polytech/cli/commands/AddStory.java
@@ -2,14 +2,17 @@ package fr.unice.polytech.cli.commands;
 
 import fr.unice.polytech.cli.framework.Command;
 import fr.unice.polytech.environment.Environment;
+import fr.unice.polytech.graphviz.Sprint;
+import fr.unice.polytech.graphviz.UserStory;
 import fr.unice.polytech.repository.DTORepository;
-import fr.unice.polytech.repository.dto.SprintDTO;
-import fr.unice.polytech.repository.dto.SprintWithStoriesDTO;
-import fr.unice.polytech.repository.dto.StoryDTO;
+import fr.unice.polytech.web.CmdException;
+import fr.unice.polytech.web.WebCommand;
+
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class AddStory extends Command<Environment> {
+public class AddStory extends Command<Environment> implements WebCommand{
 
     private String sprintName;
     private List<String> storyNames;
@@ -18,51 +21,48 @@ public class AddStory extends Command<Environment> {
     public String identifier() { return "add_story"; }
 
     @Override
-    public void execute() {
-        DTORepository repository = this.shell.system.getRepository();
-
-        SprintWithStoriesDTO sprint = repository.getSprintWithStories(sprintName);
-        if(sprint == null){
-            print("Le sprint "+ sprintName+" n'existe pas");
-            return;
-        }
-        /*
-
-        List<StoryDTO> stories = repository.getStoriesIn(storyNames);
-        if(stories.size() != storyNames.size()){
-             List<String> temp = stories.stream().map(StoryDTO::getName).collect(Collectors.toList());
-            String sts = storyNames.stream().filter(x -> !temp.contains(x)).collect(Collectors.joining(", "));
-            print("Les stories "+sts+" n'existent pas");
-            return;
-        }
-
-
-        List<StoryDTO> backlog = shell.system.getRepository().getBacklog();
-        List<StoryDTO> toAdd = stories.stream().filter( x -> !sprint.getStories().contains(x)).collect(Collectors.toList());
-        List<StoryDTO> alreadyBounded = toAdd.stream().filter(x ->!backlog.contains(x)).collect(Collectors.toList());
-        if (! alreadyBounded.isEmpty()){
-            String sts = alreadyBounded.stream().map(StoryDTO::getName).collect(Collectors.joining(", "));
-            print("Les stories "+ sts + " sont déja sélectionnées dans d'autres sprints");
-            return;
-        }
-
-
-        System.out.println();
-        List<StoryDTO> contained = stories.stream().filter( x -> sprint.getStories().contains(x)).collect(Collectors.toList());
-        if(! contained.isEmpty()){
-            String sts = contained.stream().map(StoryDTO::getName).collect(Collectors.joining(", "));
-            System.out.println("Les stories " + sts + " appartiennent déja au sprint " + sprintName + "\n");
-        }
-
-        if(! toAdd.isEmpty()){
-            addStories(sprint,toAdd);
-            String sts = toAdd.stream().map(StoryDTO::getName).collect(Collectors.joining(", "));
-            System.out.println("Les stories "+ sts + " ont été ajoutés au sprint " + sprintName);
-            System.out.println();
-        }
-        */
+    public Response execResponse() throws CmdException {
+        return Response.ok(executeWithResponse()).build();
     }
 
+    @Override
+    public void execute() {
+        print(executeWithResponse());
+    }
+
+    private String executeWithResponse(){
+        DTORepository repository = DTORepository.get();
+
+        Sprint sprint = repository.getSprintWithUserStories(sprintName);
+        if(sprint == null){
+            return "Le sprint " + sprintName + " n'existe pas";
+        }
+
+        List<UserStory> stories = repository.getStoriesIn(storyNames);
+        if(stories.size() != storyNames.size()){
+            List<String> temp = stories.stream().map(UserStory::getName).collect(Collectors.toList());
+            String sts = storyNames.stream().filter(x -> !temp.contains(x)).collect(Collectors.joining(", "));
+            return "Les stories " + sts + " n'existent pas";
+        }
+
+        List<UserStory> backlog = repository.getBacklog();
+        List<UserStory> toAdd = stories.stream().filter( x -> !sprint.getStoryList().contains(x)).collect(Collectors.toList());
+        List<UserStory> alreadyBounded = toAdd.stream().filter(x ->!backlog.contains(x)).collect(Collectors.toList());
+        if (! alreadyBounded.isEmpty()){
+            String sts = alreadyBounded.stream().map(UserStory::getName).collect(Collectors.joining(", "));
+            return "Les stories " + sts + " sont déja sélectionnées dans d'autres sprints";
+        }
+
+        List<UserStory> contained = stories.stream().filter( x -> sprint.getStoryList().contains(x)).collect(Collectors.toList());
+        if(! contained.isEmpty()){
+            String sts = contained.stream().map(UserStory::getName).collect(Collectors.joining(", "));
+            return "Les stories " + sts + " appartiennent déja au sprint " + sprintName + "\n";
+        }
+
+        addStories(sprint, toAdd);
+        String sts = toAdd.stream().map(UserStory::getName).collect(Collectors.joining(", "));
+        return "Les stories "+ sts + " ont été ajoutés au sprint " + sprintName;
+    }
 
     @Override
     public void load(List<String> args) {
@@ -71,7 +71,7 @@ public class AddStory extends Command<Environment> {
             sprintName = args.get(0).trim();
             storyNames = args.subList(1,args.size()).stream().map(String::trim).collect(Collectors.toList());
         }else {
-            print("Nombre d'arguments incorrect incorrect");
+            print("Nombre d'arguments incorrect");
         }
     }
 
@@ -85,23 +85,22 @@ public class AddStory extends Command<Environment> {
     public boolean shouldContinue() { return true; }
 
 
-    private void addStories(SprintWithStoriesDTO sprint, List<StoryDTO> toAdd) {
+    private void addStories(Sprint sprint, List<UserStory> toAdd) {
         StringBuilder sb = new StringBuilder();
-        SprintDTO sp = sprint.getSprint();
-        StoryDTO story;
+        UserStory story;
         for (int i = 0; i < toAdd.size(); i++) {
             story = toAdd.get(i);
             sb.append("MATCH (st"+i+":Story{name:"+ ss(story.getName()) +"})").append("\n");
         }
 
-        sb.append("MATCH (sp:Sprint{name:"+ss(sp.getName())+"})").append("\n");;
+        sb.append("MATCH (sp:Sprint{name:"+ss(sprint.getName())+"})").append("\n");;
 
         for (int i = 0; i < toAdd.size(); i++) {
             sb.append("CREATE (sp) -[:CONTAINS]-> (st"+i+")");
         }
 
         String query = sb.toString();
-        this.shell.system.getRepository().executeQuery(query);
+        DTORepository.get().executeQuery(query);
     }
 
 
--- main/src/main/java/fr/unice/polytech/repository/DTORepository.java
+++ main/src/main/java/fr/unice/polytech/repository/DTORepository.java
@@ -1,12 +1,13 @@
 package fr.unice.polytech.repository;
 
+import fr.unice.polytech.graphviz.Class;
+import fr.unice.polytech.graphviz.Method;
 import fr.unice.polytech.graphviz.Sprint;
 import fr.unice.polytech.graphviz.UserStory;
 import fr.unice.polytech.repository.dto.SprintDTO;
 import fr.unice.polytech.repository.dto.SprintStatDTO;
 import fr.unice.polytech.repository.dto.SprintWithStoriesDTO;
 import fr.unice.polytech.repository.dto.StoryDTO;
-import fr.unice.polytech.stories.Story;
 import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
@@ -106,9 +107,6 @@ public class DTORepository {
         }
     }
 
-
-
-
     public List<UserStory> getBacklog(){
         try (Session session = db.getDriver().session()) {
             StatementResult s = session.writeTransaction(tx -> tx.run("MATCH (s:Story) WHERE NOT (s)<-[:CONTAINS]-(:Sprint) return s"));
@@ -131,21 +129,16 @@ public class DTORepository {
            return null;
        }
 
-       sprint.fill(db.getDriver().session());
+       this.fill(sprint);
        return sprint;
     }
 
 
     public List<UserStory> getBacklogUserStories(){
         List<UserStory> backlog = getBacklog();
-        Session session = db.getDriver().session();
 
-        List<UserStory> userStories = backlog.stream()
-                        .map(x -> new UserStory(x.getName()))
-                        .collect(Collectors.toList());
-
-        userStories.forEach(x -> x.fill(session));
-        return userStories;
+        backlog.forEach(this::fill);
+        return backlog;
     }
 
     //Visitor fill
@@ -155,9 +148,21 @@ public class DTORepository {
                 tx -> tx.run(
                         "MATCH (s)<-[:CONTAINS]-(n:Sprint {name:\"" + sprint.getName() + "\"}) RETURN s"));
 
-        List<UserStory> storyList = findStories.list(story -> createUserStoryFromRequest(story.get("s")));
+        sprint.setStoryList(findStories.list(story -> createUserStoryFromRequest(story.get("s"))));
+    }
 
-        sprint.setStoryList(storyList);
+    public void fill(UserStory story){
+        StatementResult findClasses = this.getDb().getDriver().session().writeTransaction(
+                tx -> tx.run(
+                        "MATCH (c:Class)<-[:INVOLVES]-(n:Story {name:\"" + story.getName() + "\"}) RETURN c"));
+
+        story.setClasses(findClasses.list(classElement -> new Class(classElement.get("c").get("name").asString())));
+
+        StatementResult findMethods = this.getDb().getDriver().session().writeTransaction(
+                tx -> tx.run(
+                        "MATCH (c:RelationShip)<-[:INVOLVES]-(n:Story {name:\"" + story.getName() + "\"}) RETURN c"));
+
+        story.setMethods(findMethods.list(methodElement -> new Method(methodElement.get("c").get("name").asString())));
     }
 
 }
