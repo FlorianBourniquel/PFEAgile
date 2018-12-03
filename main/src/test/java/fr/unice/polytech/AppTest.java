0585f65e643097fc15cf6b24e5355f7ad2697448
David Niokhor Sene <david.n.sene@gmail.com>
2018-12-03 15:13:28 +0000

    fixed error in apptest

--- main/src/test/java/fr/unice/polytech/AppTest.java
+++ main/src/test/java/fr/unice/polytech/AppTest.java
@@ -28,26 +28,7 @@ public class AppTest extends TestCase
 
     public void testApp()
     {
-        System.out.println(executeCommand("./src/test/resources/multi/parse_stories.sh"));
-    }
-
-
-    protected String executeCommand(String command) {
-        StringBuilder output = new StringBuilder();
-        Process p;
-        try {
-            p = Runtime.getRuntime().exec(command);
-            p.waitFor();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return output.toString();
 
     }
+    
 }
