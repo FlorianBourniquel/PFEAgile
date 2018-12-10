4e603916722ac3dd9a68cf3e569cbdfe4aea93d5
David Niokhor Sene <david.n.sene@gmail.com>
2018-12-10 15:48:51 +0000

    implements display stories involving classes

--- front_end/Agile/src/app/shared/services/commands/list-stories-involving-class.service.ts
+++ front_end/Agile/src/app/shared/services/commands/list-stories-involving-class.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {CmdRequestModel} from '../../models/CmdRequestModel';
 import {UserStory} from '../../models/UserStory';
 import {Command} from './Command';
+import {Sprint} from '../../models/Sprint';
 
 @Injectable({
   providedIn: 'root'
@@ -24,8 +25,41 @@ export class ListStoriesInvolvingClassService extends Command {
   }
 
   printResponse(stories: UserStory[]) {
-    const output = stories.map(s => '[' + s.name + '] ' + s.text).join('\n');
-    alert(output);
+     const data = new CmdRequestModel('list_sprint', []);
+     this.http.post<Sprint[]>(this.base_url, data, { observe: 'response', responseType: 'json'})
+          .subscribe( x => {
+              let sprints = x.body;
+              let notInBacklog = [];
+              sprints.forEach( sp => sp.storyList = sp.storyList.filter( st => this.storyIn(st,stories, notInBacklog)));
+              const backlog: Sprint = {
+                  name: 'backlog',
+                  storyList: []
+              };
+              backlog.storyList = stories.filter( st => ! this.storyIn2(st, notInBacklog));
+              sprints.push(backlog);
+
+              sprints = sprints.filter(sp => sp.storyList.length > 0);
+              const output = sprints.map(s => this.sprintToString(s)).join('\n');
+              alert(output);
+          });
+  }
+
+   private sprintToString(sprint: Sprint): string {
+        let s = sprint.name + '\n';
+            s = s + sprint.storyList.map(us => ' [' + us.name + '] ' + us.text).join('\n');
+        return s;
+   }
+
+   private storyIn2(story: UserStory, list: UserStory[]): boolean {
+        return  list.find(value => value.name === story.name) !== undefined;
+    }
+
+  private storyIn(story: UserStory, list: UserStory[], notInbacklog: UserStory[]): boolean {
+      let res =  list.find(value => value.name === story.name) !== undefined;
+      if(res === true) {
+          notInbacklog.push(story);
+      }
+      return res;
   }
 
   identifier(): string {
